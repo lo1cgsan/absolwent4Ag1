a88d765647d7e771cb746f0b25788a748cb3714e
lo1cgsan <lo1cgsan@gmail.com>
2022-10-21 09:03:03 +0200

    dodanie szablonu bazowego

--- osoby/urls.py
+++ osoby/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'osoby'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='lista'),
     path('info/', views.info, name='info'),
 ]
